8258d0e4f8a6b1b33b65d5d001f7c864bf26a743
gaellebega <gaellebega@gmail.com>
2025-10-06 23:03:29 +0200

    few explanations added

--- home.js
+++ home.js
@@ -183,7 +183,15 @@ let min=1;
 let max=100;
 console.log(Math.random(min,max));
 
-//this is the formula to generate the random number athat are between 2 values
+//this is the formula to generate the random number that are between 2 values
 //which is gonna produce the floating number that are between those 2
-Math.random()*(maximum-minimum) + minimum;
+Math.random()*(max-min) + min;
 //example in the usage
+//This differs from the Math.floor() and Math.ceil() methods, which round down and up to the nearest whole integer, respectively.
+
+//Create a new variable called numRounded and assign the result of rounding the number 2.7. Then, log the value of numRounded to the console.
+//Below that, create another new variable called numRounded2 and assign the result of rounding the number 11.2. Then, log the value of numRounded2 to the console.
+const numRounded=Math.round(2.7);
+console.log(numRounded);
+const numRounded2=Math.round(11.2);
+console.log(numRounded2);
