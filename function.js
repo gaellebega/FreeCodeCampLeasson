257be4dbdcfb7d3afdabc99fc2feffe796370057
gaellebega <gaellebega@gmail.com>
2025-10-10 23:51:09 +0200

    new note on arrow functions

--- function.js
+++ function.js
@@ -12,3 +12,25 @@ greetings("aline");
 
 //arrow functions
 //when 
+function greetingsUs(name1){
+  console.log("Hello"+name1+"hope everything is fine!")
+}
+greetingsUs();
+//with the arrow function we are gonna make it look like this
+  greetingUs = name2=> console.log("Hello"+name2+"hope everything is fine!")
+greetingUs("uwimana");
+//the reason why it was not working is bcz you can not work with the arroe functions and then add the word function they use let const and var
+
+//here there is a small example of using the calculatoins
+
+function calculateArea(w,h){
+console.log("the area is "+w*h+"cm"+(2**1))
+}
+calculateArea(2,3);
+
+
+const calculateAreaa=(w,h)=>{
+  return "the area is "+w*h+"cm"+(2**1);
+}
+//when we have used the return then to be able to see our output we need to console the function itself
+console.log(calculateAreaa(5,8));
