c94517566fdb11152ec3467ab78fca06be139e18
gaellebega <gaellebega@gmail.com>
2025-10-10 23:06:55 +0200

    anonymus,arrow function I

---
+++ function.js
@@ -0,0 +1,14 @@
+const sum= function(num1,num2){
+  return num1+num2;
+}
+console.log(sum(4,5));//it worked
+
+function greetings (name="Uwimana"){
+  console.log("hello"+name+"hope you are good!")
+}
+greetings();
+//here is gonna take the name as aline and also the name as uwimana
+greetings("aline");
+
+//arrow functions
+//when 
