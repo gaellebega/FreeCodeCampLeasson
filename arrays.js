de629c4618f358831db2d30ee5d2b7e43afccafe
gaellebega <gaellebega@gmail.com>
2025-10-25 14:42:02 +0200

    arrays

---
+++ arrays.js
@@ -0,0 +1,15 @@
+//arrays are the ordered list of the elements
+
+let arrays=["uwimana","uwase","uwabo","uwundi","umushaka"]
+console.log(arrays.length);
+
+console.log(arrays[3]);
+
+console.log(arrays[3]="muhawenimana");
+//to return the index of an element that does not exist you get  an error
+console.log(arrays.push("gasana"))
+console.log(arrays.pop());
+console.log(arrays)
+console.log(arrays.unshift("Mukeshimana"));
+console.log(arrays.shift());//
+console.log(arrays);
