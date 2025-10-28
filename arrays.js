4d462fffdc24fc27cd73ebfe2f0fa7c18fdb67ba
gaellebega <gaellebega@gmail.com>
2025-10-28 22:13:28 +0200

    split,join,reverse method added on file

--- arrays.js
+++ arrays.js
@@ -13,3 +13,22 @@ console.log(arrays)
 console.log(arrays.unshift("Mukeshimana"));
 console.log(arrays.shift());//
 console.log(arrays);
+
+//when we have this sign
+const colors = ["red", "green", "blue"];
+
+let [firstColor, , thirdColor] = colors;
+
+console.log(firstColor); // "red"
+console.log(thirdColor); // "blue"
+ // we also have something that they call the rest which is used to provide the remaining set of the arrays
+
+ //how to use the  split reverse  and join methods in js
+
+ let word="holla madrid";
+ //when there is no character provided is gonna provide the variable the way it is
+ newsentence=word.split("");
+ console.log(newsentence)
+ console.log(newsentence.reverse());
+ console.log(newsentence.join(""));
+//  console.log(word);
