0683d06a98845c0260b581043c0a0a4a7bb82e29
gaellebega <gaellebega@gmail.com>
2025-11-08 14:31:47 +0200

    recipe tracker project

---
+++ PROJECT/receipetracker.js
@@ -0,0 +1,28 @@
+const recipes = [];
+const recipe1 = {
+  name: "spaghetti carbonara",
+  ingredients: ["Spaghetti", "Parmesan cheese", "Pancetta", "black pepper"],
+  ratings: [4, 5, 4, 5],
+};
+//add more on the recipe1
+recipe1.cookingTime = 22;
+recipe1.totalIngredients = null;
+recipe1.difficultyLevel = "";
+recipe1.averageRating = null;
+console.log(recipe1);
+
+const recipe2 = {
+  name: "Chicken Curry",
+  ingredients: [
+    "chicken breast",
+    "coconut milk",
+    "curry powder",
+    "onion",
+    "garlic",
+  ],
+  ratings: [4, 5, 5, 5],
+  cookingTime: 42,
+  totalIngredients: null,
+  difficultyLevel: "",
+  averageRating: null,
+};
