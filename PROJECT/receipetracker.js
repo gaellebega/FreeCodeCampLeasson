20277864a35c029ba17414819a43fc15c57ea6d6
gaellebega <gaellebega@gmail.com>
2025-11-10 23:43:52 +0200

    changes on the deletion of variable

--- PROJECT/receipetracker.js
+++ PROJECT/receipetracker.js
@@ -1,40 +1,23 @@
 const recipes = [];
-const recipe1 = {
-  name: "spaghetti carbonara",
-  ingredients: ["Spaghetti", "Parmesan cheese", "Pancetta", "black pepper"],
-  ratings: [4, 5, 4, 5],
-};
-//add more on the recipe1
-recipe1.cookingTime = 22;
-recipe1.totalIngredients = null;
-recipe1.difficultyLevel = "";
-recipe1.averageRating = null;
-console.log(recipe1);
 
-const recipe2 = {
-  name: "Chicken Curry",
-  ingredients: [
-    "chicken breast",
-    "coconut milk",
-    "curry powder",
-    "onion",
-    "garlic",
-  ],
-  ratings: [4, 5, 5, 5],
-  cookingTime: 42,
-  totalIngredients: null,
-  difficultyLevel: "",
-  averageRating: null,
-};
-// Step 1: Create recipe1
 const recipe1 = {
-  name: "Pasta Bolognese",
-  ingredients: ["pasta", "ground beef", "tomato sauce", "onion", "garlic"],
-  ratings: [5, 4, 4, 5],
-  cookingTime: 30,
+  name: 'Spaghetti Carbonara',
+  ingredients: ['spaghetti', 'Parmesan cheese', 'pancetta', 'black pepper'],
+  cookingTime: 22,
   totalIngredients: null,
   difficultyLevel: '',
-  averageRating: null
+  ratings: [4, 5, 4, 5],
+  averageRating: null,
+};
+
+const recipe2 = {
+  name: 'Chicken Curry',
+  ingredients: ['chicken breast', 'coconut milk', 'curry powder', 'onion', 'garlic'],
+  cookingTime: 42,
+  totalIngredients: null,
+  difficultyLevel: '',
+  ratings: [4, 5, 5, 5],
+  averageRating: null,
 };
 
 const recipe3 = {
@@ -47,15 +30,6 @@ const recipe3 = {
   averageRating: null,
 };
 
-// Step 3: Access properties
-const recipe1Name = recipe1.name;
-const recipe2Name = recipe2.name;
-const recipe1CookingTime = recipe1.cookingTime;
-const recipe2CookingTime = recipe2.cookingTime;
 
-// Step 4: Log everything
-console.log(recipe1Name);
-console.log(recipe2Name);
-console.log(recipe1CookingTime);
-console.log(recipe2CookingTime);
-recipes.push(recipe1,recipe2,recipe3)
+recipes.push(recipe1,recipe2,recipe3);
+console.log(recipes);
